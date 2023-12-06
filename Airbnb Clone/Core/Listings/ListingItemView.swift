a9eec427094628b1136506c21a7a7cc1d5e1397b
sebastian-nunez <02snunez@gmail.com>
2023-12-05 23:05:06 -0500

    added actual mock images for listings

--- Airbnb Clone/Core/Listings/ListingItemView.swift
+++ Airbnb Clone/Core/Listings/ListingItemView.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct ListingItemView: View {
+    // TODO: replace with actual images
+    var images = [
+        "listing-1",
+        "listing-2",
+        "listing-3",
+        "listing-4"
+    ]
+    
     var body: some View {
         VStack(spacing: 8) {
             // images
-            Rectangle()
-                .frame(height: 320)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+            TabView {
+                ForEach(images, id: \.self) { image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
             
             // listing info
             HStack(alignment: .top) {
