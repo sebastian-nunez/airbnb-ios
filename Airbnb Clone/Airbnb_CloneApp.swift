040efd9b0a84bde5fcb7476f4e5723c44108fe43
sebastian-nunez <02snunez@gmail.com>
2023-12-05 22:01:50 -0500

    initial commit

---
+++ Airbnb Clone/Airbnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_CloneApp.swift
+//  Airbnb Clone
+//
+//  Created by Sebastian on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Airbnb Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Airbnb Clone
+//
+//  Created by Sebastian on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
