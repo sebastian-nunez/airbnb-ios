c5a2fc0e541ebd1d3292ba11e7c3a17919891dd2
sebastian-nunez <02snunez@gmail.com>
2023-12-05 22:35:33 -0500

    Add ListingItemView.swift to project

---
+++ Airbnb Clone/Core/Listings/ListingItemView.swift
@@ -0,0 +1,56 @@
+//
+//  ListingItemView.swift
+//  Airbnb Clone
+//
+//  Created by Sebastian on 12/5/23.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    var body: some View {
+        VStack(spacing: 8) {
+            // images
+            Rectangle()
+                .frame(width: UIScreen.main.bounds.width - 20, height: 320)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            // listing info
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                    Text("Miami, Florida")
+                        .fontWeight(.bold)
+                    
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                    
+                    HStack(spacing: 4) {
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        
+                        Text("night")
+                    }
+                }
+                
+                Spacer()
+                
+                // ratings
+                HStack(spacing: 4) {
+                    Image(systemName: "star.fill")
+                    
+                    Text("4.86")
+                        .fontWeight(.bold)
+                }
+            }
+            .font(.footnote)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
