0f21e22a00b2666cf55d3ac4da4683f86444f0f9
sebastian-nunez <02snunez@gmail.com>
2023-12-07 00:20:35 -0500

    created search bar filter for explore view

---
+++ Airbnb Clone/Core/Explore/SearchFilterBarView.swift
@@ -0,0 +1,51 @@
+//
+//  SearchFilterBarView.swift
+//  Airbnb Clone
+//
+//  Created by Sebastian on 12/6/23.
+//
+
+import SwiftUI
+
+struct SearchFilterBarView: View {
+    var body: some View {
+        HStack {
+            HStack(spacing: 12) {
+                // image
+                Image(systemName: "magnifyingglass")
+
+                // location info
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("Where to?")
+                        .font(.footnote)
+                        .fontWeight(.bold)
+
+                    Text("Anywhere - Any Week - Add Guests")
+                        .font(.caption2)
+                        .foregroundStyle(.gray)
+                }
+            }
+
+            Spacer()
+
+            // filter
+            Button {} label: {
+                Image(systemName: "line.3.horizontal.decrease.circle")
+                    .foregroundStyle(.black)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .overlay {
+            Capsule()
+                .stroke(lineWidth: 0.5)
+                .foregroundStyle(Color(.systemGray3))
+                .shadow(color: .black.opacity(0.2), radius: 2)
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    SearchFilterBarView()
+}
