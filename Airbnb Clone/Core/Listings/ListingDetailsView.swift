38b1ba17dd0e87c23ae8cf056f018e50e3054c33
sebastian-nunez <02snunez@gmail.com>
2023-12-07 01:25:41 -0500

    began working on the property details

--- Airbnb Clone/Core/Listings/ListingDetailsView.swift
+++ Airbnb Clone/Core/Listings/ListingDetailsView.swift
@@ -15,6 +15,34 @@ struct ListingDetailsView: View {
                               cornerRadius: 0)
 
             // property details
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Miami Villa")
+                    .font(.title)
+                    .bold()
+
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack(spacing: 2) {
+                        // ratings
+                        Image(systemName: "star.fill")
+                        Text("4.86")
+                            .fontWeight(.bold)
+
+                        Text(" - ")
+
+                        // reviews
+                        Text("28 reviews")
+                            .fontWeight(.semibold)
+                            .underline()
+                    }
+                    .foregroundStyle(.black)
+
+                    // location
+                    Text("Miami, Florida")
+                }
+                .font(.caption)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading) // align left
+            .padding(.leading)
 
             // owner info
 
