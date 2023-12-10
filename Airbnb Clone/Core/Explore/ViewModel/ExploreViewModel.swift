8d9a14f0beb9562f0978ab612042dfca118cf713
sebastian-nunez <02snunez@gmail.com>
2023-12-09 19:38:43 -0500

    Add filterListingByLocation method to ExploreViewModel

--- Airbnb Clone/Core/Explore/ViewModel/ExploreViewModel.swift
+++ Airbnb Clone/Core/Explore/ViewModel/ExploreViewModel.swift
@@ -26,4 +26,18 @@ class ExploreViewModel: ObservableObject {
             print("DEBUG: failed to fetch listings with error \(error.localizedDescription)")
         }
     }
+
+    func filterListingByLocation(for location: String) {
+        let normalizedLocation = location.lowercased()
+
+        let filteredListings = listings.filter { listing in
+            let normalizedCity = listing.city.lowercased()
+            let normalizedState = listing.state.lowercased()
+
+            return normalizedCity.starts(with: normalizedLocation) ||
+                normalizedState.starts(with: normalizedLocation)
+        }
+
+        listings = filteredListings.isEmpty ? listings : filteredListings
+    }
 }
